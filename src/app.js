da544cdd95d40303e9b4c2260beaf5dcb7ee19ba
LovishGoyal01 <goyallovish852@gmail.com>
2025-07-16 22:02:22 +0530

    exploring post get delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express =require("express");
 
 const app = express();
 
-app.use("/",(req,res) => {
+app.get("/",(req,res) => {
     res.send("Hello world Main");
 })
 
-app.use("/test",(req,res) => {
-    res.send("Hello world test");
+app.get("/test",(req,res) => {
+    res.send("Haahaa testing");
 })
 
-app.use("/hello",(req,res) => {
-    res.send("Hello world Hello");
+app.get("/user",(req,res) => {
+    res.send({firstname:"Lovish",lastname:"goyal"});
+})
+
+app.post("/user",(req,res) => {
+    //Server to DB
+    res.send("Data submitted successfully");
 })
 
 app.listen(1505,()=>{
